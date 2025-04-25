440a92d8c88f6dd0bdeaec7011e0adb8aa68efde
lin <lin.sitian@outlook.com>
2025-04-25 09:44:18 +0800

    initialize and config env

---
+++ src/main/java/org/example/datamonitoringengine/DataMonitoringEngineApplication.java
@@ -0,0 +1,13 @@
+package org.example.datamonitoringengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMonitoringEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataMonitoringEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/datamonitoringengine/DataMonitoringEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.datamonitoringengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMonitoringEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
